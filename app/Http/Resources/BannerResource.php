8718eabd06849ee4e7de139833120278f60f0cab
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-02-23 05:00:02 +0200

    add id category resourse

--- app/Http/Resources/BannerResource.php
+++ app/Http/Resources/BannerResource.php
@@ -15,6 +15,7 @@ class BannerResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id'        => $this->id,
             'name'      => $this->name,
             'map'       => $this->map,
             'image'     => $this->image_path,
